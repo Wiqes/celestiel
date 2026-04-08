b8ce059cb7a4e0452d2470e6e0d36caf926cb18a
KSkryabin <k.skryabin@mobidev.biz>
2026-04-08 20:17:52 +0300

    dep

--- js/36.js
+++ js/36.js
@@ -1,1 +1,116 @@
-import{a as p}from"./0.js";import{c as l,d as f}from"./3.js";import{a as u,b as g}from"./6.js";import{a as b}from"./25.js";import{T as r,Z as i,a as h,b as v,ua as o,yc as m,zc as d}from"./34.js";var n=class s{interlocutor=o(null);changeOnlineStatus(e,t){let c=this.interlocutor();c&&c.id===e&&this.interlocutor.set(v(h({},c),{isOnline:t}))}static \u0275fac=function(t){return new(t||s)};static \u0275prov=r({token:s,factory:s.\u0275fac,providedIn:"root"})};var a={INVALID_TOKEN:"Invalid token provided for socket",NO_TOKEN:"No token provided for socket",TOKEN_IS_EXPIRED:"Token is expired",RATE_LIMIT_EXCEEDED:"Rate limit exceeded"};var k=class s{interlocutorService=i(n);socket=i(p);authTokenService=i(f);chatStateService=i(l);messageService=i(g);authTokenStateService=i(b);token=m(()=>this.authTokenStateService.token());isChatsLoading=o(!1);constructor(){this.subscribeToEvents(),d(()=>{let e=this.token();e&&(this.disconnect(),this.connect(e))})}subscribeToEvents(){this.onUserChats().subscribe(e=>{this.chatStateService.updateChats(e),this.isChatsLoading.set(!1)}),this.onUserOnlineStatus().subscribe(e=>{this.chatStateService.updateUserOnlineStatus(e.userId,e.isOnline),this.interlocutorService.changeOnlineStatus(e.userId,e.isOnline)}),this.onMessageRemoved().subscribe(e=>{this.chatStateService.removeMessageFromChat(e.chatId,e.messageId)}),this.onError().subscribe(e=>{e.message===a.INVALID_TOKEN&&this.authTokenService.refreshToken().subscribe(t=>{this.connect(t)}),e.message===a.RATE_LIMIT_EXCEEDED&&(this.disconnect(),this.authTokenService.logout(),setTimeout(()=>{this.messageService.sendMessage(u.TOO_MANY_REQUESTS)},2e3))}),this.onDisconnect().subscribe(()=>{this.chatStateService.chats.set(null)})}isConnected(){return this.socket.ioSocket.connected}connect(e){this.isConnected()||(e&&(this.socket.ioSocket.auth={token:e}),this.socket.connect())}disconnect(){this.socket.disconnect()}sendMessage(e){this.socket.emit("sendMessage",e)}onReceiveMessage(){return this.socket.fromEvent("receiveMessage")}removeMessage(e){this.socket.emit("removeMessage",{messageId:e})}onMessageRemoved(){return this.socket.fromEvent("messageRemoved")}onUserOnlineStatus(){return this.socket.fromEvent("userOnlineStatus")}joinChat(e,t){return this.socket.emit("joinChat",{participant1Id:e,participant2Id:t}),this.socket.fromEvent("joinChatResponse")}leaveChat(e){this.socket.emit("leaveChat",{chatId:e})}markMessageAsRead(e){this.socket.emit("markMessageAsRead",{chatId:e})}sendTypingIndicator(e,t){this.socket.emit("typing",{chatId:e,isTyping:t})}onUserTyping(){return this.socket.fromEvent("userTyping")}onUserChats(){return this.socket.fromEvent("userChats")}getUserChats(){this.socket.emit("getUserChats")}onPreviousMessages(){return this.socket.fromEvent("previousMessages")}onConnect(){return this.socket.fromEvent("connect")}onDisconnect(){return this.socket.fromEvent("disconnect")}onError(){return this.socket.fromEvent("error")}static \u0275fac=function(t){return new(t||s)};static \u0275prov=r({token:s,factory:s.\u0275fac,providedIn:"root"})};export{n as a,k as b};
+import{Ea as A,Fa as g,Ia as q,La as R,j as N,m as Q,q as V,r as O,v as z}from"./50.js";import{$b as F,Cb as l,Db as p,Eb as I,Hb as M,Nb as m,Qb as v,Sb as y,T as _,Ta as a,Tb as h,U as w,Yb as B,Z as T,dc as P,fb as k,fc as D,gb as x,gc as $,ia as b,jb as S,kb as f,rb as s,sb as r,tb as c,vc as j,wb as u,wc as E}from"./31.js";var H=["content"],J=(e,o)=>({"p-progressbar p-component":!0,"p-progressbar-determinate":e,"p-progressbar-indeterminate":o}),K=e=>({$implicit:e});function L(e,o){if(e&1&&(l(0,"div"),B(1),p()),e&2){let t=m(2);c("display",t.value!=null&&t.value!==0?"flex":"none"),s("data-pc-section","label"),a(),F("",t.value,"",t.unit,"")}}function U(e,o){e&1&&M(0)}function W(e,o){if(e&1&&(l(0,"div",3)(1,"div",4),f(2,L,2,5,"div",5)(3,U,1,0,"ng-container",6),p()()),e&2){let t=m();u(t.valueStyleClass),c("width",t.value+"%")("background",t.color),r("ngClass","p-progressbar-value p-progressbar-value-animate"),s("data-pc-section","value"),a(2),r("ngIf",t.showValue&&!t.contentTemplate&&!t._contentTemplate),a(),r("ngTemplateOutlet",t.contentTemplate||t._contentTemplate)("ngTemplateOutletContext",D(11,K,t.value))}}function X(e,o){if(e&1&&(l(0,"div",7),I(1,"div",8),p()),e&2){let t=m();u(t.valueStyleClass),r("ngClass","p-progressbar-indeterminate-container"),s("data-pc-section","container"),a(),c("background",t.color),s("data-pc-section","value")}}var Y=({dt:e})=>`
+.p-progressbar {
+    position: relative;
+    overflow: hidden;
+    height: ${e("progressbar.height")};
+    background: ${e("progressbar.background")};
+    border-radius: ${e("progressbar.border.radius")};
+}
+
+.p-progressbar-value {
+    margin: 0;
+    background: ${e("progressbar.value.background")};
+}
+
+.p-progressbar-label {
+    color: ${e("progressbar.label.color")};
+    font-size: ${e("progressbar.label.font.size")};
+    font-weight: ${e("progressbar.label.font.weight")};
+}
+
+.p-progressbar-determinate .p-progressbar-value {
+    height: 100%;
+    width: 0%;
+    position: absolute;
+    display: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    transition: width 1s ease-in-out;
+}
+
+.p-progressbar-determinate .p-progressbar-label {
+    display: inline-flex;
+}
+
+.p-progressbar-indeterminate .p-progressbar-value::before {
+    content: "";
+    position: absolute;
+    background: inherit;
+    top: 0;
+    inset-inline-start: 0;
+    bottom: 0;
+    will-change: inset-inline-start, inset-inline-end;
+    animation: p-progressbar-indeterminate-anim 2.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) infinite;
+}
+
+.p-progressbar-indeterminate .p-progressbar-value::after {
+    content: "";
+    position: absolute;
+    background: inherit;
+    top: 0;
+    inset-inline-start: 0;
+    bottom: 0;
+    will-change: inset-inline-start, inset-inline-end;
+    animation: p-progressbar-indeterminate-anim-short 2.1s cubic-bezier(0.165, 0.84, 0.44, 1) infinite;
+    animation-delay: 1.15s;
+}
+
+@-webkit-keyframes p-progressbar-indeterminate-anim {
+    0% {
+        inset-inline-start: -35%;
+        inset-inline-end: 100%;
+    }
+    60% {
+        inset-inline-start: 100%;
+        inset-inline-end: -90%;
+    }
+    100% {
+        inset-inline-start: 100%;
+        inset-inline-end: -90%;
+    }
+}
+@keyframes p-progressbar-indeterminate-anim {
+    0% {
+        inset-inline-start: -35%;
+        inset-inline-end: 100%;
+    }
+    60% {
+        inset-inline-start: 100%;
+        inset-inline-end: -90%;
+    }
+    100% {
+        inset-inline-start: 100%;
+        inset-inline-end: -90%;
+    }
+}
+@-webkit-keyframes p-progressbar-indeterminate-anim-short {
+    0% {
+        inset-inline-start: -200%;
+        inset-inline-end: 100%;
+    }
+    60% {
+        inset-inline-start: 107%;
+        inset-inline-end: -8%;
+    }
+    100% {
+        inset-inline-start: 107%;
+        inset-inline-end: -8%;
+    }
+}
+@keyframes p-progressbar-indeterminate-anim-short {
+    0% {
+        inset-inline-start: -200%;
+        inset-inline-end: 100%;
+    }
+    60% {
+        inset-inline-start: 107%;
+        inset-inline-end: -8%;
+    }
+    100% {
+        inset-inline-start: 107%;
+        inset-inline-end: -8%;
+    }
+}
+`,Z={root:({instance:e})=>["p-progressbar p-component",{"p-progressbar-determinate":e.determinate,"p-progressbar-indeterminate":e.indeterminate}],value:"p-progressbar-value",label:"p-progressbar-label"},G=(()=>{class e extends q{name="progressbar";theme=Y;classes=Z;static \u0275fac=(()=>{let t;return function(n){return(t||(t=b(e)))(n||e)}})();static \u0275prov=_({token:e,factory:e.\u0275fac})}return e})();var ee=(()=>{class e extends R{value;showValue=!0;styleClass;valueStyleClass;style;unit="%";mode="determinate";color;contentTemplate;_componentStyle=T(G);templates;_contentTemplate;ngAfterContentInit(){this.templates?.forEach(t=>{switch(t.getType()){case"content":this._contentTemplate=t.template;break;default:this._contentTemplate=t.template}})}static \u0275fac=(()=>{let t;return function(n){return(t||(t=b(e)))(n||e)}})();static \u0275cmp=k({type:e,selectors:[["p-progressBar"],["p-progressbar"],["p-progress-bar"]],contentQueries:function(i,n,C){if(i&1&&(v(C,H,4),v(C,A,4)),i&2){let d;y(d=h())&&(n.contentTemplate=d.first),y(d=h())&&(n.templates=d)}},inputs:{value:[2,"value","value",E],showValue:[2,"showValue","showValue",j],styleClass:"styleClass",valueStyleClass:"valueStyleClass",style:"style",unit:"unit",mode:"mode",color:"color"},features:[P([G]),S],decls:3,vars:15,consts:[["role","progressbar",3,"ngStyle","ngClass"],["style","display:flex",3,"ngClass","class","width","background",4,"ngIf"],[3,"ngClass","class",4,"ngIf"],[2,"display","flex",3,"ngClass"],[1,"p-progressbar-label"],[3,"display",4,"ngIf"],[4,"ngTemplateOutlet","ngTemplateOutletContext"],[3,"ngClass"],[1,"p-progressbar-value","p-progressbar-value-animate"]],template:function(i,n){i&1&&(l(0,"div",0),f(1,W,4,13,"div",1)(2,X,2,7,"div",2),p()),i&2&&(u(n.styleClass),r("ngStyle",n.style)("ngClass",$(12,J,n.mode==="determinate",n.mode==="indeterminate")),s("aria-valuemin",0)("aria-valuenow",n.value)("aria-valuemax",100)("data-pc-name","progressbar")("data-pc-section","root")("aria-label",n.value+n.unit),a(),r("ngIf",n.mode==="determinate"),a(),r("ngIf",n.mode==="indeterminate"))},dependencies:[z,N,Q,O,V,g],encapsulation:2,changeDetection:0})}return e})(),fe=(()=>{class e{static \u0275fac=function(i){return new(i||e)};static \u0275mod=x({type:e});static \u0275inj=w({imports:[ee,g,g]})}return e})();export{ee as a,fe as b};
